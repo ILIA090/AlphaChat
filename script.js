b95ede7cd604f63382f359bf928a2c58e5a36153
ILIA0 <155063101+ILIA090@users.noreply.github.com>
2026-01-08 15:02:09 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+// آدرس WebSocket سرورت (Render)
+const ws = new WebSocket("wss://YOUR-RENDER-URL/ws");
 
+const messagesDiv = document.getElementById("messages");
+
+ws.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+
+  const div = document.createElement("div");
+  div.className = "msg";
+  div.innerHTML = `<span>${data.user}</span><br>${data.text}`;
+  messagesDiv.appendChild(div);
+
+  messagesDiv.scrollTop = messagesDiv.scrollHeight;
+};
+
+function sendMessage() {
+  const user = document.getElementById("username").value || "Guest";
+  const text = document.getElementById("message").value;
+  if (!text) return;
+
+  ws.send(JSON.stringify({
+    user: user,
+    text: text
+  }));
+
+  document.getElementById("message").value = "";
+}
